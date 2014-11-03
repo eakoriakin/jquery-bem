12fdd5d426ab2e8f6dc1aaf752cb482f2fa86b2f
Evgeny K. <eakoryakin@gmail.com>
2014-11-03 16:13:28 +0500

    Adding blockModifier and splitModifiers functions

--- Bem.js
+++ Bem.js
@@ -1,8 +1,9 @@
-$(function () {
+﻿$(function () {
     // Bem static members.
     var bem = {};
     window.bem = bem;
 
+    // Fields.
     bem.elementSeparator = "__";
     bem.modifierSeparator = "_";
     bem.modifierValueSeparator = "_";
@@ -49,25 +50,120 @@ $(function () {
     //    return $.trim(resultingCssClass);
     //};
 
+    // Methods.
+    bem.block = function () {
+        if (arguments.length === 2 && typeof arguments[0] === "string" && typeof arguments[1] === "string") {
+        }
+        else if (arguments.length === 3 && typeof arguments[0] === "string" && typeof arguments[1] === "string" && typeof arguments[2] === "string") {
+        }
+        else if (arguments.length === 2 && typeof arguments[0] === "string" && $.isArray(arguments[1])) {
+        }
+        else if (arguments.length === 3 && typeof arguments[0] === "string" && $.isArray(arguments[1]) && $.isArray(arguments[2])) {
+        }
+    };
+
+    bem.blockModifier = function () {
+        /// <signature>
+        /// <summary>
+        /// Creates block modifier CSS-class.
+        /// </summary>
+        /// <param name="blockName" type="String">Block name.</param>
+        /// <param name="modifier" type="String">One or more modifiers, separated by a space.</param>
+        /// <returns type="String">Block modifier CSS-class.</returns>
+        /// </signature>
+        /// <signature>
+        /// <summary>
+        /// Creates block modifier CSS-class.
+        /// </summary>
+        /// <param name="blockName" type="String">Block name.</param>
+        /// <param name="modifierName" type="String">Modifier name.</param>
+        /// <param name="modifierValue" type="String">Modifier value.</param>
+        /// <returns type="String">Block modifier CSS-class.</returns>
+        /// </signature>
+        /// <signature>
+        /// <summary>
+        /// Creates block modifier CSS-class.
+        /// </summary>
+        /// <param name="blockName" type="String">Block name.</param>
+        /// <param name="modifiers" type="Array">List of modifiers.</param>
+        /// <returns type="String">Block modifier CSS-class.</returns>
+        /// </signature>
+        if (arguments.length === 2 && typeof arguments[0] === "string" && typeof arguments[1] === "string") {
+            var blockName = $.trim(arguments[0]),
+                modifier = $.trim(arguments[1]);
+
+            if (isNullOrWhiteSpace(blockName) || isNullOrWhiteSpace(modifier)) {
+                return "";
+            }
+
+            return bem.blockModifier(blockName, bem.splitModifiers(modifier));
+        }
+        else if (arguments.length === 3 && typeof arguments[0] === "string" && typeof arguments[1] === "string" && typeof arguments[2] === "string") {
+            var blockName = $.trim(arguments[0]),
+                modifierName = $.trim(arguments[1]),
+                modifierValue = $.trim(arguments[2]);
+
+            if (isNullOrWhiteSpace(blockName) || isNullOrWhiteSpace(modifierName) || isNullOrWhiteSpace(modifierValue)) {
+                return "";
+            }
+
+            return blockName + bem.modifierSeparator + modifierName + bem.modifierValueSeparator + modifierValue;
+        }
+        else if (arguments.length === 2 && typeof arguments[0] === "string" && $.isArray(arguments[1])) {
+            var blockName = $.trim(arguments[0]),
+                modifiers = arguments[1];
+
+            if (isNullOrWhiteSpace(blockName) || modifiers.length == 0) {
+                return "";
+            }
+
+            var resultingCssClass = new StringBuilder();
+
+            for (var i = 0; i < modifiers.length; i++) {
+                var modifier = modifiers[i];
+    
+                if (isNullOrWhiteSpace(modifier.name)) {
+                    continue;
+                }
+
+                if (isNullOrWhiteSpace(modifier.value))
+                {
+                    resultingCssClass.append(blockName + bem.modifierSeparator + modifier.name + " ");
+                }
+
+                else
+                {
+                    resultingCssClass.append(blockName + bem.modifierSeparator + modifier.name + bem.modifierValueSeparator + modifier.value + " ");
+                }
+            }
+
+            return $.trim(resultingCssClass.toString());
+        }
+        else {
+            return "";
+        }
+    };
+
     bem.splitCssClasses = function (cssClasses) {
-    	/// <summary>
-        /// Creates list of CSS-classes from string.
+        /// <summary>
+        /// Creates a list of CSS-classes from the string.
         /// <para>Removes duplicate CSS-classes.</para>
-    	/// </summary>
+        /// </summary>
         /// <param name="cssClasses" type="String">One or more CSS-classes, separated by a space.</param>
-        /// <returns type="Array">List of CSS-classes.</returns>
+        /// <returns type="Array">A list of CSS-classes.</returns>
         var newCssClasses = [];
+        cssClasses = $.trim(cssClasses);
 
         if (!cssClasses) {
             return newCssClasses;
         }
 
-        var splittedCssClasses = $.trim(cssClasses).split(" ");
+        var splittedCssClasses = cssClasses.split(" ");
 
         for (var i = 0; i < splittedCssClasses.length; i++) {
             var cssClass = splittedCssClasses[i],
                 cssClassExists = false;
-            
+
             for (var j = 0; j < newCssClasses.length; j++) {
                 if (cssClass === newCssClasses[j]) {
                     cssClassExists = true;
@@ -75,13 +171,78 @@ $(function () {
                 }
             }
 
-            if (cssClass && !cssClassExists) {
+            if (!isNullOrWhiteSpace(cssClass) && !cssClassExists) {
                 newCssClasses.push(cssClass);
             }
         }
 
         return newCssClasses;
+    };
+
+    bem.splitModifiers = function (modifiers) {
+        /// <summary>
+        /// Creates a list of modifiers from the string.
+        /// <para>If the string contains the same modifiers but with different values​​, then the value of the last modifier is taken.</para>
+        /// </summary>
+        /// <param name="modifiers" type="String">One or more modifiers, separated by a space.</param>
+        /// <returns type="Array">A list of modifiers.</returns>
+        var newModifiers = [];
+        modifiers = $.trim(modifiers);
+
+        if (!modifiers) {
+            return newModifiers;
+        }
+
+        var splittedModifiers = modifiers.split(" ");
+
+        for (var i = 0; i < splittedModifiers.length; i++) {
+            var modifier = splittedModifiers[i],
+                modifierParts = modifier.split(bem.modifierValueSeparator),
+                modifierName = modifierParts[0],
+                modifierValue = modifierParts.length == 2 ? modifierParts[1] : "",
+                modifierExists = false;
+
+            for (var j = 0; j < newModifiers.length; j++) {
+                if (modifierName === newModifiers[j].name) {
+                    newModifiers[j].value = modifierValue;
+                    modifierExists = true;
+                    break;
+                }
+            }
+
+            if (!modifierExists) {
+                newModifiers.push({
+                    name: modifierName,
+                    value: modifierValue
+                });
+            }
+        }
+
+        return newModifiers;
     }
+
+    // Methods (private).
+    function isNullOrWhiteSpace(value) {
+        if (value === null || value === undefined) {
+            return true;
+        }
+
+        return value.replace(/\s/g, '').length < 1;
+    };
+
+    // Nested types.
+    function StringBuilder() {
+        this.strings = [];
+
+        this.append = function (value) {
+            this.strings.push(value);
+            return this;
+        };
+
+        this.toString = function () {
+            return this.strings.join("");
+        }
+    };
 
     // Bem jQuery-extension.
     var bemExtension = function () {
--- Tests.js
+++ Tests.js
@@ -1,4 +1,22 @@
-test("SplitCssClasses", function () {
+test("blockModifier", function () {
+    // Act & Assert.
+    strictEqual(
+        bem.blockModifier("block", "is-enabled is-focused size_xs"),
+        "block_is-enabled block_is-focused block_size_xs",
+        "Creates block modifier CSS-class with multiple modifiers."
+    );
+});
+
+test("blockModifier", function () {
+    // Act & Assert.
+    strictEqual(
+        bem.blockModifier("block", "size", "xs"),
+        "block_size_xs",
+        "Creates block modifier CSS-class with single modifier with value."
+    );
+});
+
+test("splitCssClasses", function () {
     // Arrange.
     var serializedCssClasses = "h3 h1 h2 h1 h3";
     var expectedCssClasses = ["h3", "h1", "h2"]
@@ -6,6 +24,38 @@ test("SplitCssClasses", function () {
     // Act.
     var actualCssClasses = bem.splitCssClasses(serializedCssClasses);
 
-    // Act & Assert.
+    // Assert.
     deepEqual(actualCssClasses, expectedCssClasses, "Splits CSS-classes and removes duplicates.");
+});
+
+test("splitModifiers", function () {
+    // Arrange.
+    var serializedModifiers = "is-selected size_xs is-focused";
+    var expectedModifiers = [
+        { name: "is-selected", value: "" },
+        { name: "size", value: "xs" },
+        { name: "is-focused", value: "" }
+    ];
+
+    // Act.
+    var actualModifiers = bem.splitModifiers(serializedModifiers);
+
+    // Assert.
+    deepEqual(actualModifiers, expectedModifiers, "Splits modifiers.");
+});
+
+test("splitModifiers", function () {
+    // Arrange.
+    var serializedModifiers = "is-selected size_xs is-focused size_m is-selected";
+    var expectedModifiers = [
+        { name: "is-selected", value: "" },
+        { name: "size", value: "m" },
+        { name: "is-focused", value: "" }
+    ];
+
+    // Act.
+    var actualModifiers = bem.splitModifiers(serializedModifiers);
+
+    // Assert.
+    deepEqual(actualModifiers, expectedModifiers, "Gets only the last modifier from multiple duplicates.");
 });
